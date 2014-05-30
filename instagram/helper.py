f27495991bc358ca382ca02a81382e3277e9b685
Jay Johnston <jay@4st.org>
2014-05-30 14:33:00 -0400

    Force UTC tzinfo

--- instagram/helper.py
+++ instagram/helper.py
@@ -1,9 +1,9 @@
 import calendar
 from datetime import datetime
-
+from django.utils.timezone import utc
 
 def timestamp_to_datetime(ts):
-    return datetime.utcfromtimestamp(float(ts))
+    return datetime.utcfromtimestamp(float(ts)).replace(tzinfo=utc)
 
 
 def datetime_to_timestamp(dt):
